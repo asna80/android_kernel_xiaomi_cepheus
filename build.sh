298c7eb76afb9ebb65b90d2ba3ef46d569ca9dc1
asna80 <168886236+asna80@users.noreply.github.com>
2024-06-06 12:55:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 
 KERNEL_DEFCONFIG=cepheus_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_cepheus_A12-A13_v1.14.zip
+FINAL_KERNEL_ZIP=Asuna_cepheus_DLN.zip
 
 # paths
 TC="$PWD"
 
-PATH=${TC}/clang-r416183b1/bin:${TC}/aarch64/bin:${TC}/arm/bin:$PATH
+PATH=${TC}/clang+llvm-17.0.2-x86_64-linux-gnu-ubuntu-22.04/bin:${TC}/aarch64/bin:${TC}/arm/bin:$PATH
 
 export LLVM=1
 export CC=clang
@@ -27,7 +27,7 @@ START=$(date +"%s")
 make ARCH=arm64 \
         O=out \
         CC=clang \
-		AR=llvm-ar \
+	AR=llvm-ar \
         LD=ld.lld \
         NM=llvm-nm \
         OBJCOPY=llvm-objcopy \
@@ -38,6 +38,7 @@ make ARCH=arm64 \
 
 echo -e "$yellow**** Verify Image.gz-dtb ****$nocol"
 ls $PWD/out/arch/arm64/boot/Image.gz-dtb
+mkdir $PWD/kernel_out
 
 echo -e "$yellow**** Verifying AnyKernel3 Directory ****$nocol"
 ls $ANYKERNEL3_DIR
@@ -51,7 +52,7 @@ cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
 echo -e "$yellow**** Time to zip up! ****$nocol"
 cd $ANYKERNEL3_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP /home/raystef66/kernel/$FINAL_KERNEL_ZIP
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $PWD/kernel_out/$FINAL_KERNEL_ZIP
 
 echo -e "$yellow**** Done, here is your checksum ****$nocol"
 cd ..
